3cc814219b4bcdbba64169e5c3dacffc869747fd
Cerus <zocks1703@web.de>
2020-10-26 04:13:50 +0100

    Initial commit

---
+++ src/main/java/de/cerus/simplenbt/tag/Tag.java
@@ -0,0 +1,21 @@
+package de.cerus.simplenbt.tag;
+
+public abstract class Tag<T> {
+
+    private final String name;
+    private final T value;
+
+    protected Tag(final String name, final T value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public T getValue() {
+        return this.value;
+    }
+
+}
