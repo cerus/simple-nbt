8604c4defaeff5e6c9e35cb115801a8fd54c254a
Cerus <zocks1703@web.de>
2020-10-26 04:17:46 +0100

    Add TAG_End

---
+++ src/main/java/de/cerus/simplenbt/tag/TagEnd.java
@@ -0,0 +1,12 @@
+package de.cerus.simplenbt.tag;
+
+/**
+ * TAG_End has no name and no value.
+ */
+public class TagEnd extends Tag<Void> {
+
+    protected TagEnd(final String name, final Void value) {
+        super(name, value);
+    }
+
+}
